040c39ba70a96b0cf49e3dc75be514ad70dba24c
georgiangelov2000 <60781748+georgiangelov2000@users.noreply.github.com>
2020-12-22 16:40:16 +0200

    Exercise

--- JAVASCRIPT/BASIC/First character of the given string added at the front and back.js
+++
@@ -1,8 +0,0 @@
-function solve(string) {
-    string = string.split('');
-    let element = string[0];
-    string[string.length - 1] = element;
-    return string.join('');
-}
-let result = solve('something');
-console.log(result)
--- JAVASCRIPT/BASIC/Two integers values are in the range 50,99 – Копие.js
+++
@@ -1,9 +0,0 @@
-function solve(a, b) {
-    if (a >= 50 && a <= 99 || b >= 50 && b <= 99) {
-        return true;
-    } else {
-        return false
-    }
-}
-let result=solve(21,82);
-console.log(result)
