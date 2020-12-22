4a9e9f68b6f2d7c85abcbfa43b3bbc803c0fc2ce
georgiangelov2000 <60781748+georgiangelov2000@users.noreply.github.com>
2020-12-22 23:23:59 +0200

    Exercise

--- JAVASCRIPT/BASIC/Conditional-Statements/Check from two given integers whether one of them is 8 or their sum or difference is 8.js
+++ JAVASCRIPT/BASIC/Conditional-Statements/Check from two given integers whether one of them is 8 or their sum or difference is 8.js
@@ -1,6 +1,5 @@
 function solve(a, b) {
-    let sum = a - b;
-    if (a == 8 || b == 8 || a + -b == 8) {
+    if (a == 8 || b == 8 || a  -b == 8) {
         return true;
     } else {
         return false
