aef44fbf455afbd72d81f293cf8c94d0b2e6da62
georgiangelov2000 <60781748+georgiangelov2000@users.noreply.github.com>
2020-12-28 18:52:10 +0200

    Web exercise

--- JAVASCRIPT/ADVANCED/Dom/05. Chat-Room/solution.js
+++ JAVASCRIPT/ADVANCED/Dom/05. Chat-Room/solution.js
@@ -1,5 +1,14 @@
 function solve() {
-   //TODO...
+   let sendMessage = document.getElementById('send');
+   let inputMessage = document.getElementById('chat_input')
+   let chatMessage = document.getElementById('chat_messages');
+   sendMessage.addEventListener('click', function (e) {
+      let messsage = inputMessage.value;
+      let newMessage = document.createElement('div');
+      newMessage.classList.add('message');
+      newMessage.classList.add('my-message');
+      newMessage.textContent = messsage;
+      chatMessage.appendChild(newMessage);
+      inputMessage.value = ''
+   })
 }
-
-
