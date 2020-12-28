27c6af810484b309bc684c978d466a773742cc18
georgiangelov2000 <60781748+georgiangelov2000@users.noreply.github.com>
2020-12-28 09:15:29 +0200

    Web exercise

--- JAVASCRIPT/ADVANCED/Dom/07. Number-Convertor/solution.js
+++ JAVASCRIPT/ADVANCED/Dom/07. Number-Convertor/solution.js
@@ -1,4 +1,28 @@
 function solve() {
+    let inputNumber = document.getElementById('input');
+    let selectTo = document.getElementById('selectMenuTo');
+    let result = document.getElementById('result');
+    let optionBinary = document.createElement('option');
+    optionBinary.value = 'binary';
+    optionBinary.innerHTML = 'Binary';
+    let optionHexadecimal = document.createElement('option');
+    optionHexadecimal.value = 'hexadecimal';
+    optionHexadecimal.innerHTML = 'Hexadecimal';
+    selectTo.appendChild(optionBinary);
+    selectTo.appendChild(optionHexadecimal)
 
-    //TODO...
+    let resButton = document.getElementsByTagName('button')[0];
+
+    resButton.addEventListener('click', convert);
+
+    function convert() {
+        if (selectTo.value === 'binary') {
+            result.value = Number(inputNumber.value).toString(2);
+        } else if (selectTo.value === 'hexadecimal') {
+            result.value = Number(inputNumber.value).toString(16)
+        }else{
+            result.value=''
+            console.log(result)
+        }
+    }
 }
